37957ea1547e2754096a7f487b4cb6958f25c541
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 17:47:42 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,8 +0,0 @@
-file_exists "import-map.json"
-file_exists "README.md"
-file_contains "LICENSE" "Apache License"
-
-file_exists "dist/mxClient.js"
-dir_exists "dist/css"
-dir_exists "dist/images"
-dir_exists "dist/resources"
